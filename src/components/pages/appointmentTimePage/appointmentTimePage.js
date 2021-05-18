927cd4b9fa11a2786cc081b6f93576db51314e6d
SIDLESS <pronkodg@gmail.com>
2021-05-18 16:29:09 +0300

    Final TimePage start

--- src/components/pages/appointmentTimePage/appointmentTimePage.js
+++ src/components/pages/appointmentTimePage/appointmentTimePage.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {addDate} from '../../../redux/actions';
 import DB from '../../../service/service';
 
 
-function TimePage(props) {
+class TimePage extends Component {
 
-    const db = new DB();
+    constructor(props) {
+        super(props);
+        this.state = {
+            mstLoaded: false
+        }
+    }
+    db = new DB();
 
-    function renderTime(ms) {
+    formatTime(ms) {
         const hours = Math.floor(ms/1000/60/60)
         const minutes = Math.floor(ms/(1000*60)%60)
         return `${hours}:${(minutes < 10) ? '0' + minutes : minutes}`
     }
 
-    function render() {
+    renderButtons() {
         let arr = [];
         const startTime = 32400000,
               endTime = 75600000,
@@ -27,34 +33,40 @@ function TimePage(props) {
                 <Link to='/appointment' key={i}>
                     <button className='btn'
                             key={i}
-                            onClick={() => props.addDate(props.date.setMilliseconds(i))}>{renderTime(i)}</button>
+                            onClick={() => this.props.addDate(this.props.date.setMilliseconds(i))}>{this.formatTime(i)}</button>
                 </Link>
             )
         }
-        
         return arr
     }
 
-    // function getMinServiceTime() {
-    //     console.log('getMinServiceTime function')
-    //     let arr2 = []
-    //     db.getAllServices().then(res => {
-    //         res.forEach(value => arr2.push(value.time))
-    //     });
-    //     console.log(arr2)
-    //     console.log('getMinServiceTime function end')
-    //     // arr.forEach(value => arr2.push(value.time))
-    //     // console.log(arr2.sort().shift())
-    //     // return arr2.sort().shift()
-    // }
-    
+    getMinServiceTime() {
+        this.db.getAllServices().then(res => {
+            // res.map(value => value.time).sort()[0]
+            console.log(res)
+            this.setState({mstLoaded: true})
+        })
+    }
 
-    return (
-        <>
-            <h1>Время</h1>
-            {render()}
-        </>
-    )
+    
+    render() {
+
+        if (this.state.mstLoaded) {
+            return (
+                <>
+                    <h1>Время</h1>
+                    {this.renderButtons()}
+                </>
+            )
+        } else {
+            this.getMinServiceTime()
+            return (
+                <div className="progress">
+                    <div className="indeterminate"></div>
+                </div>
+            )
+        }
+    }
 }
 
 const mapStateToProps = state => {
