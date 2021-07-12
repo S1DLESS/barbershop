7256b04f9b1fcde3d03313a6438fb84e77359a8b
SIDLESS <example@whatever.com>
2021-07-12 18:38:02 +0300

    index update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const {connect} = require('mongoose')
 const router = require('./routes')
 const cors = require('cors')
+const path = require('path')
 
 
 const PORT = process.env.PORT || 5000
@@ -12,6 +13,12 @@ app.use(cors())
 app.use('/static', express.static('static'))
 app.use(express.json())
 app.use('/', router)
+
+if (process.env.NODE_ENV === 'production') {
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const start = async () => {
     try {
